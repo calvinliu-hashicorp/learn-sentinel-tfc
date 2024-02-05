88e0cc962b98484858e21bbfcada23021294c57a
Calvin Liu <116752643+calvinliu-hashicorp@users.noreply.github.com>
2024-02-05 16:53:14 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,11 +18,18 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "random_pet" "name" {
+  length    = 3
+  separator = "-"
+  prefix    = "pet"
+}
+
 resource "aws_instance" "ubuntu" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
 
   tags = {
     Name = var.instance_name
+    Pet = "${random_pet.name.id}"
   }
 }
